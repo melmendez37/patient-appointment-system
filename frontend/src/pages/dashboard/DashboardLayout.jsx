80b348f631c3e1de1cb89356efbbf3aac4dcbc44
Mel Angelo Mendez <mel.mendez37@gmail.com>
2026-01-26 00:13:45 +0800

    fix: redirect to dashboard

--- frontend/src/pages/dashboard/DashboardLayout.jsx
+++ frontend/src/pages/dashboard/DashboardLayout.jsx
@@ -1,12 +1,15 @@
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
 import { Outlet } from "react-router-dom";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 const DashboardLayout = () => {
+  const {user} = useAuthContext();
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      {!user && (<div className="flex min-h-screen gap-6">
+      {user && (<div className="flex min-h-screen gap-6">
         <Sidebar />
         <Outlet/>
       </div>)}
