b03462253097c05a17f42f75774bead7f8d3a505
Mel Angelo Mendez <mel.mendez37@gmail.com>
2026-01-26 00:11:15 +0800

    fix: login/logout hook redirects using navigate

--- frontend/src/hooks/useLogin.jsx
+++ frontend/src/hooks/useLogin.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const { dispatch } = useAuthContext();
+    const navigate = useNavigate();
 
     const login = async (email, password) => {
         setIsLoading(true);
@@ -37,6 +39,7 @@ export const useLogin = () => {
                 dispatch({ type: 'LOGIN', payload: json }); 
 
                 setIsLoading(false);
+                navigate("/dashboard", {replace: true});
             }
         } catch (error) {
             setError('Network error. Please try again later.');
